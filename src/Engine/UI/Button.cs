5ac3400562d5c15611c4dd99d8f93ea021047492
kenny <kenny@DESKTOP-LHLDSM7>
2023-06-01 13:53:23 +0300

    button

---
+++ src/Engine/UI/Button.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameEngine.src;
+using System;
+
+namespace MonoGameEngine.src.Engine
+{
+	internal class Button : Drawable
+	{
+		Texture2D mask;
+		private bool drawMask = false;
+		public Button(Texture2D _mask) { setMask(_mask); }
+		public void update()
+		{
+			if (Bounds().Intersects(new Rectangle(InputManager.getMouseCoordinates().ToPoint(), new Point(1, 1))))
+			{
+				drawMask = true;
+				if (InputManager.isMouseButtonPressed(MOUSE_BUTTON.LEFT))
+				{
+					onClick();
+				}
+			}
+			else
+			{
+				drawMask = false;
+			}
+		}
+		internal void onClick()
+		{
+			
+		}
+		internal void setMask(Texture2D _mask)
+		{
+			mask = _mask;
+		}
+
+		public override void Draw()
+		{
+			base.Draw();
+
+			if (drawMask)
+			{
+				Texture2D temp = Texture;
+				Texture = mask;
+				Render.Draw(this);
+				Texture = temp;
+			}
+
+		}
+	}
+}
